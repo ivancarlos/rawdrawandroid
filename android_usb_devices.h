266e11fb11730d262681218fe9755ff35fa7bf6e
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-08 15:42:46 -0300

    :style: Atualiza o estilo do arquivo android_usb_devices.h

--- android_usb_devices.h
+++ android_usb_devices.h
@@ -1,4 +1,5 @@
-//Copyright 2020 <>< Charles Lohr, You may use this file and library freely under the MIT/x11, NewBSD or ColorChord Licenses.
+// Copyright 2020 <>< Charles Lohr, You may use this file and
+// library freely under the MIT/x11, NewBSD or ColorChord Licenses.
 
 #ifndef _ANDROID_USB_DEVICES_H
 #define _ANDROID_USB_DEVICES_H
@@ -7,11 +8,12 @@
 #include <asset_manager_jni.h>
 #include <android_native_app_glue.h>
 
-int RequestPermissionOrGetConnectionFD( char * debug_status, uint16_t vid, uint16_t pid );
-void DisconnectUSB(); //Disconnect from USB
+int  RequestPermissionOrGetConnectionFD(char*    debug_status,
+                                        uint16_t vid,
+                                        uint16_t pid);
+void DisconnectUSB();  // Disconnect from USB
 
 extern jobject deviceConnection;
-extern int deviceConnectionFD;
+extern int     deviceConnectionFD;
 
 #endif
-
